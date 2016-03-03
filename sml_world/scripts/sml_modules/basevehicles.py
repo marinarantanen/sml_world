d45af9d6d1a77e76b182bc73b21e3350e7bbbbe3
davidspulak <spulak@kth.se>
2016-03-03 16:53:46 +0100

    Add class variables, simulation_loop(), simulation_step().
    Also comment out service that is not yet implemented.
    
    In simulation loop try to achieve simulation rate, otherwise log warning message.

--- sml_world/scripts/sml_modules/basevehicles.py
+++ sml_world/scripts/sml_modules/basevehicles.py
@@ -1,9 +1,11 @@
 """Base classes for simulated vehicles."""
+import time
+import random
 
 import rospy
 from std_msgs.msg import String
 from sml_world.msg import VehicleState
-from sml_world.srv import PublishCom
+# from sml_world.srv import PublishCom
 
 from sml_modules.bodyclasses import WheeledVehicle
 
@@ -11,7 +13,7 @@ from sml_modules.bodyclasses import WheeledVehicle
 class BaseVehicle(WheeledVehicle):
     """Base class for all vehicles."""
 
-    def __init__(self, namespace):
+    def __init__(self, namespace, vehicle_id, simulation_rate):
         """Initialize class BaseVehicle."""
         rospy.Subscriber(namespace + '/sensor_readings', String,
                          self.process_sensor_readings)
@@ -21,9 +23,39 @@ class BaseVehicle(WheeledVehicle):
         self.pub_state = rospy.Publisher(namespace + "/current_vehicle_state",
                                          VehicleState, queue_size=10)
 
-        rospy.wait_for_service(namespace + '/publish_com')
-        self.publish_com = rospy.ServiceProxy(namespace + '/publish_com',
-                                              PublishCom)
+        # rospy.wait_for_service(namespace + '/publish_com')
+        # self.publish_com = rospy.ServiceProxy(namespace + '/publish_com',
+        #                                       PublishCom)
+
+        self.vehicle_id = vehicle_id
+        self.simulation_rate = simulation_rate
+        self.sensors = []
+
+        self.simulation_loop()
+
+    def simulation_loop(self):
+        """The simulation loop of the car."""
+        rate = rospy.Rate(self.simulation_rate)
+        while not rospy.is_shutdown():
+            self.simulation_step()
+            if rate.remaining() < rospy.Duration(0):
+                rospy.logwarn("Simulation rate of vehicle " +
+                              "#%i " % self.vehicle_id +
+                              "could not be achieved.")
+                rate.last_time = rospy.get_rostime()
+            else:
+                rate.sleep()
+
+    def simulation_step(self):
+        """Simulate one timestep of the car."""
+        # Find closest trajectory point.
+
+        # Set reference point 5 indices ahead of the closest trajecctory point
+        # to imporove lateral controller performance.
+        # Get reference state.
+
+        # Drive at the cruise velocity.
+        time.sleep(random.random() / 2.)
 
     def process_sensor_readings(self, data):
         """Process all sensor readings."""
