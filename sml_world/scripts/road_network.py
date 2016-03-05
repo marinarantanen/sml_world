44cb64e6b98b8d344e54a15ec7171bbf95f2c3cb
davidspulak <spulak@kth.se>
2016-03-06 00:46:25 +0100

    Remove old debugging output.

--- sml_world/scripts/road_network.py
+++ sml_world/scripts/road_network.py
@@ -80,7 +80,6 @@ class RoadModuleExtend(RoadModule):
             # yaw 0..2pi
             if yaw < 0.:
                 yaw += 2*math.pi
-                print "->", yaw
             trajectory.append(Pose2D(x, y, yaw))
         return GetTrajectoryResponse(trajectory)
 
