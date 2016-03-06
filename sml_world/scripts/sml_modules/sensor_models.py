db3f44879fa70254128a83732afccd57927c2116
davidspulak <spulak@kth.se>
2016-03-06 20:17:47 +0100

    Expand class BaseSensor with class variable vehicle_id; Save also ids and speeds in vehicle_states class variable

--- sml_world/scripts/sml_modules/sensor_models.py
+++ sml_world/scripts/sml_modules/sensor_models.py
@@ -11,25 +11,28 @@ from std_msgs.msg import String
 class BaseSensor(object):
     """Base class for sensors."""
 
-    def __init__(self):
+    def __init__(self, vehicle_id):
         """Initialize class BaseSensor."""
-        self.vehicle_poses = numpy.asarray([[], [], []])
+        self.vehicle_id = vehicle_id
+        self.vehicle_poses = numpy.asarray([[], [], [], [], []])
         rospy.Subscriber('/world_state', WorldState, self.update_state)
 
     def update_state(self, ws):
         """Callback function for topic 'world_state'."""
-        self.vehicle_poses = numpy.asarray([[], [], []])
+        self.vehicle_poses = numpy.asarray([[], [], [], [], []])
         for vs in ws.vehicle_states:
-            numpy.concatenate((self.vehicle_poses, [[vs.x], [vs.y], [vs.yaw]]),
+            numpy.concatenate((self.vehicle_poses,
+                               [[vs.vehicle_id], [vs.x], [vs.y],
+                                [vs.yaw], [vs.v]]),
                               axis=1)
 
 
 class Radar(BaseSensor):
     """Radar sensor class."""
 
-    def __init__(self, name):
+    def __init__(self, vehicle_id, name):
         """Initialize class."""
-        super(Radar, self).__init__()
+        super(Radar, self).__init__(vehicle_id)
         self.name = name
         self.pub_readings = rospy.Publisher(self.name, String,
                                             queue_size=10)
