20774ae21e573eccfaa9ca7ec8a3d304c6a4d526
davidspulak <spulak@kth.se>
2016-03-08 10:45:49 +0100

    Implement Radar publish_readings() function.

--- sml_world/scripts/sml_modules/sensor_models.py
+++ sml_world/scripts/sml_modules/sensor_models.py
@@ -43,7 +43,39 @@ class Radar(BaseSensor):
 
     def publish_readings(self):
         """Publish the sensor readings."""
-        self.pub_readings.publish("These are the sensor readings!")
+        # Only go through this routine if vehicle is in state array.
+        if (not np.any(self.vehicle_states[0] == self.vehicle_id) or
+            not self.vehicle_states.shape[1] > 1):
+            return
+        # Get the current vehicle state.
+        ind = self.vehicle_states[0].tolist().index(self.vehicle_id)
+        self_state = self.vehicle_states[:, ind]
+        vehicle_states = np.delete(self.vehicle_states, np.s_[ind], 1)
+        rel_state = (vehicle_states[(1, 2, 3), :].transpose() -
+                     self_state[[1, 2, 3]]).transpose()
+        # Calculate rotation matrix
+        yaw = self_state[3]
+        rot = np.asmatrix([[np.cos(yaw), np.sin(yaw)],
+                           [-np.sin(yaw), np.cos(yaw)]])
+        # Transform current vehicle states.
+        rel_pos = rot * rel_state[(0, 1), :]
+        # Find states in range.
+        to_polar = np.vectorize(lambda x, y: (np.hypot(x, y),
+                                              np.arctan2(y, x)))
+        rel_rho, rel_theta = to_polar(rel_pos[0, :], rel_pos[1, :])
+        pol_state = np.concatenate((np.array(rel_rho),
+                                    np.array(rel_theta),
+                                    [rel_state[2, :]]),
+                                   axis=0)
+        pol_state = pol_state[:, np.all(
+                                  (pol_state[0, :] <= self.sens_range,
+                                   np.abs(pol_state[1, :]) <= self.sens_angle),
+                                  axis=0)]
+        # Send out message with relative poses.
+        radar_readings = []
+        for v in pol_state.transpose():
+            radar_readings.append(Pose2DPolar(v[0], v[1], v[2]))
+        self.pub_readings.publish(RadarReadings(radar_readings))
 
 
 class Velodyne(BaseSensor):
